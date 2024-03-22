15bfc4fcb19df6ea51359bd81698966d6c8aa455
RickyTickyTavy69 <bakirovartem69@gmail.com>
2024-03-22 21:54:09 +0100

    add development and prod build options to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,42 +6,50 @@ import eslintPlugin from 'vite-plugin-eslint'
 import svgr from "vite-plugin-svgr";
 import { analyzer } from "vite-bundle-analyzer";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineConfig(({command, mode}) => {
 
-export default defineConfig({
-    // depending on your application, base can also be "/"
-    plugins: import.meta.env.MODE === "development" ? [ react(),
-        svgr(),
-        analyzer({
-        analyzerMode : "server",
-        analyzerPort: 3001,
-        }),
-        eslintPlugin({
-            cache: false,
-            include: ['./src/**/*.js', './src/**/*.jsx'],
-            exclude: [],
-        }),
-    ]: [
-        svgr(),
-        eslintPlugin({
-            cache: false,
-            include: ['./src/**/*.js', './src/**/*.jsx'],
-            exclude: [],
-        }),
-    ],
-    resolve: {
-        alias: {
-            "src": "/src",
+    // this mode is set in scripts with "--mode development"
+    // it is possible to use those variables to manage builds, create
+    // production and dev builds
+    // command can be build or serve depending on if this is dev server or just a build
+
+    return {
+        // depending on your application, base can also be "/"
+        plugins: mode === "development" && command === "build" ? [ react(),
+            svgr(),
+            analyzer({
+                analyzerMode : "server",
+                analyzerPort: 3001,
+            }),
+            eslintPlugin({
+                cache: false,
+                include: ['./src/**/*.js', './src/**/*.jsx'],
+                exclude: [],
+            }),
+        ]: [
+            svgr(),
+            eslintPlugin({
+                cache: false,
+                include: ['./src/**/*.js', './src/**/*.jsx'],
+                exclude: [],
+            }),
+        ],
+        resolve: {
+            alias: {
+                "src": "/src",
+            },
         },
-    },
-    server: {
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000
-        port: 3000,
-    },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './config/vitest/setupTests.ts'
-    },
+        server: {
+            // this ensures that the browser opens upon server start
+            open: true,
+            // this sets a default port to 3000
+            port: 3000,
+        },
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './config/vitest/setupTests.ts'
+        },
+    }
 })
