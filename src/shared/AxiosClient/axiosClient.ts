4656e06aad3ed2b8d1dd9a04dfe9b38d4d469301
RickyTickyTavy69 <bakirovartem69@gmail.com>
2024-03-23 23:21:19 +0100

    adding api Client2

---
+++ src/shared/AxiosClient/axiosClient.ts
@@ -0,0 +1,32 @@
+// eslint-disable
+import axios, {AxiosInstance} from "axios";
+
+type ClientOptions = {
+    baseURL: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class AxiosClient{
+    public readonly apiInstance: AxiosInstance;
+    constructor({baseURL}: ClientOptions){
+        this.apiInstance = axios.create({baseURL})
+    }
+    // copied from work project, see how the generics work
+    public apiGet<R>(url: string, params?: unknown): Promise<R> {
+        return this.apiInstance.get(url, { params });
+    }
+
+    public apiPost() {
+
+    }
+
+    public apiPut(){
+
+    }
+
+    public apiDelete(){
+
+    }
+}
+
+console.log(AxiosClient);
