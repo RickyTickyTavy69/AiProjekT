a7407a9cb1a3c51f9aeb3fa30da3861db33c0c7b
RickyTickyTavy69 <bakirovartem69@gmail.com>
2024-03-25 00:00:05 +0100

    changed loki testing target to docker

--- dist/assets/Menu-CMua2ccO.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-Kq6Zg22M.js";const r=()=>s.jsx("div",{children:"This is Menu"});export{r as default};
--- dist/assets/Profile-CI7LEd6C.js
+++
@@ -1,1 +0,0 @@
-import{j as i,L as s}from"./index-Kq6Zg22M.js";const e=()=>i.jsxs("div",{children:[i.jsx(s,{to:"/",children:"Main"}),i.jsx("div",{children:"This is Profile"})]});export{e as default};
