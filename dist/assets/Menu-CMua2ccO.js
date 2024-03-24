69a1324882c7a5fd8b0972778cb22fc8d94576e1
RickyTickyTavy69 <bakirovartem69@gmail.com>
2024-03-24 20:40:31 +0100

    added screenshot testing to ci pipeline34

---
+++ dist/assets/Menu-CMua2ccO.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Kq6Zg22M.js";const r=()=>s.jsx("div",{children:"This is Menu"});export{r as default};
---
+++ dist/assets/Profile-CI7LEd6C.js
@@ -0,0 +1,1 @@
+import{j as i,L as s}from"./index-Kq6Zg22M.js";const e=()=>i.jsxs("div",{children:[i.jsx(s,{to:"/",children:"Main"}),i.jsx("div",{children:"This is Profile"})]});export{e as default};
