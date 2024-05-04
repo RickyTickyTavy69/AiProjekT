869556d2f37ff3a0aceabe9f7748f4460fe1f1e8
artem <bakirovartem69@gmail.com>
2024-05-04 12:58:13 +0200

    Modal in Storybook works by now

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,11 +10,13 @@ export default {
         extend: {
             colors: {
                 black: '#18181B',
-                gray: '#E5E7EB'
+                gray: '#E5E7EB',
+                overlay_color: 'rgba(24,24,27,0.6)',
+                overlay_color_bright: 'rgba(24,24,27,0.6)'
             },
             height:{
-                navbar: "50px",
-                mainContent: "calc(100% - 50px)"
+                navbar: "80px",
+                mainContent: "calc(100% - 80px)"
             }
         },
     },
