be9b1f61f582b0e58eca7bbd5d0f4df695c60eae
artem <bakirovartem69@gmail.com>
2024-06-02 19:18:40 +0200

    testing loki

--- src/shared/uiKit/Modal/Modal.tsx
+++ src/shared/uiKit/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import React, {
-    // useCallback,
+    useCallback,
     // useEffect,
     // useState
 } from "react";
@@ -9,8 +9,8 @@ import {motion} from "framer-motion";
 
 
 type ModalProps = {
-    // isOpen: boolean;
-    // onClose: () => void;
+    isOpen: boolean;
+    onClose: () => void;
     children: React.ReactNode;
     // lazy?: boolean;
     // isTest?: boolean;
@@ -18,8 +18,8 @@ type ModalProps = {
 
 const Modal = (
     {
-        // isOpen = true,
-        // onClose,
+        isOpen = true,
+        onClose,
         children,
         // lazy = false,
         // isTest = false
@@ -27,17 +27,17 @@ const Modal = (
 ) => {
   // const [isMounted, setIsMounted] = useState(false);
 
-  /* const clickHandler = useCallback( () => {
+  const clickHandler = useCallback( () => {
         if(onClose){
             onClose();
         }
     }, [onClose])
 
-    const keyDownHandler = useCallback( (e: KeyboardEvent) => {
+    /* const keyDownHandler = useCallback( (e: KeyboardEvent) => {
         if(e.key === "Escape"){
             clickHandler();
         }
-    }, [clickHandler])
+    }, [clickHandler])  */
 
     const variants = {
         open: { opacity: 1, scale: 1},
@@ -45,7 +45,7 @@ const Modal = (
     }
 
 
-    useEffect(() => {
+   /* useEffect(() => {
         if(isOpen){
             window.addEventListener("keydown", keyDownHandler);
         }
@@ -67,9 +67,9 @@ const Modal = (
   return (
     <motion.div
       data-testid={"modal-overlay"}
-      // animate={isOpen ? "open" : "closed"}
-      // variants={variants}
-      // onClick={clickHandler}
+      animate={isOpen ? "open" : "closed"}
+      variants={variants}
+      onClick={clickHandler}
       className={classNames(
         `fixed top-0 bottom-0 left-0 right-0
              flex justify-center items-center -z-10 bg-overlay_color`,
@@ -80,8 +80,8 @@ const Modal = (
     >
       <motion.div
         data-testid={"modal-window"}
-        // animate={isOpen ? "open" : "closed"}
-        // variants={variants}
+        animate={isOpen ? "open" : "closed"}
+        variants={variants}
         onClick={(e) => e.stopPropagation()}
         className={
           "bg-modal_color dark:bg-modal_color_dark text-black rounded h-52 w-96 opacity-0 p-2"
